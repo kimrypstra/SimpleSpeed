710c2d419d40de9f4ae5496fa531d9596fc1a7ab
Kim Rypstra <kim.rypstra@gmail.com>
2025-01-25 21:09:26 +0800

    Initial commit

---
+++ SimpleSpeed/SimpleSpeed/SimpleSpeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleSpeedApp.swift
+//  SimpleSpeed
+//
+//  Created by Kim Rypstra on 25/1/2025.
+//
+
+import SwiftUI
+
+@main
+struct SimpleSpeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
